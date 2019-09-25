01c5845f0dac97a211894639f26e45aea9fe774c
Diego Amaya <damaya@easysol.net>
2019-09-25 08:46:21 -0500

    First Commit: maven wrapper and base line

---
+++ src/main/java/com/waes/devassessment/DevAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.waes.devassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAssessmentApplication.class, args);
+	}
+
+}
